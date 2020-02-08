f8a18e0041e5426fb2ffef749f4dffe05a44baa7
quarnet <patelaxay47@gmail.com>
2020-02-08 20:03:08 +0530

    deploy changes

---
+++ build/precache-manifest.d92e9319eda90b21a9fb630e05204d24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68543bcf031857c20880a40c481beb54",
+    "url": "/ReactAutoSuggestionBox/index.html"
+  },
+  {
+    "revision": "99065c1dac69fa06ad6b",
+    "url": "/ReactAutoSuggestionBox/static/css/main.20afc98f.chunk.css"
+  },
+  {
+    "revision": "66c4a786a45837c63451",
+    "url": "/ReactAutoSuggestionBox/static/js/2.620addf6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ReactAutoSuggestionBox/static/js/2.620addf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99065c1dac69fa06ad6b",
+    "url": "/ReactAutoSuggestionBox/static/js/main.33817d5c.chunk.js"
+  },
+  {
+    "revision": "5e3c2e6ee1bcbed2934e",
+    "url": "/ReactAutoSuggestionBox/static/js/runtime-main.40db91c7.js"
+  }
+]);
