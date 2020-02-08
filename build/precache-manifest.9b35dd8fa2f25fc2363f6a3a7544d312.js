6f523f07c8fa7d72401561530427ebb5d8130980
quarnet <patelaxay47@gmail.com>
2020-02-08 17:56:22 +0530

    add click listner

---
+++ build/precache-manifest.9b35dd8fa2f25fc2363f6a3a7544d312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f29e3819b84f20308b957d6f8a5480",
+    "url": "/ReactAutoSuggestionBox/index.html"
+  },
+  {
+    "revision": "d1a2b7d4e16761489fea",
+    "url": "/ReactAutoSuggestionBox/static/css/main.a8542959.chunk.css"
+  },
+  {
+    "revision": "66c4a786a45837c63451",
+    "url": "/ReactAutoSuggestionBox/static/js/2.620addf6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ReactAutoSuggestionBox/static/js/2.620addf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a2b7d4e16761489fea",
+    "url": "/ReactAutoSuggestionBox/static/js/main.33817d5c.chunk.js"
+  },
+  {
+    "revision": "5e3c2e6ee1bcbed2934e",
+    "url": "/ReactAutoSuggestionBox/static/js/runtime-main.40db91c7.js"
+  }
+]);
